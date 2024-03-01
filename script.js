f819309be9ab829d69ecf960153754f3b314679e
Alice Buchholz <alice.buchholz@gmail.com>
2024-03-01 11:44:04 +0100

    Info card top background designe

--- script.js
+++ script.js
@@ -92,12 +92,26 @@ async function renderPokemonInfo(cardId) {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${cardId}`);
   const clickedPokemon = await response.json();
   // Build HTML content based on clickedPokemon information
+
+  const firstType = clickedPokemon['types'][0]; // Get the first type
+  const typeColor = TypeColors.find(color => color.type === firstType.type.name)?.color; // Find the matching color
+  const secondColor = TypeColors.find(color => color.type === firstType.type.name)?.backgroundColor;
+
+  const types = clickedPokemon['types'].map(type => {
+  const typeColor = TypeColors.find(color => color.type === type.type.name)?.color;
+  return `<div class="type" style="background-color: ${typeColor}">${type.type.name}</div>`;
+  }).join('');
+
   content.innerHTML = `
-  <div class="top-card">
-    <h2>${clickedPokemon.name}</h2>
-    <p>ID: #${clickedPokemon.id.toString().padStart(4, '0')}</p>
-  </div>
-    <img src="${clickedPokemon['sprites']['other']['official-artwork']['front_default']}" alt="${clickedPokemon.name}">
+  <div class="info-card-top" style="background-image: linear-gradient(to bottom, ${typeColor}, ${secondColor});">
+    <div class="top-card">
+      <h2>${clickedPokemon.name}</h2>
+      <p>ID: #${clickedPokemon.id.toString().padStart(4, '0')}</p>
+    </div>
+    <div class="types">${types}</div>
+    </div>
+      <img src="${clickedPokemon['sprites']['other']['official-artwork']['front_default']}" alt="${clickedPokemon.name}">
+  
     `;
 }
 
